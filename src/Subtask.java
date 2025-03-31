8e45e80d5bc75bcb66343c0434aa0643b0bde725
aisha-miranda <tashchanova@bk.ru>
2025-03-31 22:34:07 +0500

    Реализован класс subtask

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Subtask extends Task{
+    private Epic epic;
 
-public class Subtask {
+    public Subtask(String title, String description, Epic epic) {
+        super(title, description);
+        this.epic = epic;
+    }
+
+    @Override
+    public void setStatus(String status) {
+        super.setStatus(status);
+        epic.updateStatus();
+    }
+
+    public Epic getEpic() {
+        return epic;
+    }
 }
