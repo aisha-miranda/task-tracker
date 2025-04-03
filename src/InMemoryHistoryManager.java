d2370919430b26b5ed3e08589b59ec21e4fed7ec
aisha-miranda <tashchanova@bk.ru>
2025-04-03 23:48:28 +0500

    Реализованы методы интерфейса HistoryManager, добавлены поля history (список истории просмотров) и MAX_HISTORY_SIZE (максимальное кол-во элементов в списке)

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class inMemoryHistoryManager implements HistoryManager {
+    private static final int MAX_HISTORY_SIZE = 10;
+    private final List<Task> history = new ArrayList<>();
+
+    @Override
+    public void add(Task task) {
+        if (task != null) {
+            // Если размер в истории достиг максимального, удаляем самую старую задачу
+            if (history.size() >= MAX_HISTORY_SIZE) {
+                history.remove(0);
+            }
+            history.add(task);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return List.copyOf(history);
+    }
+}
