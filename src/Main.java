770927692cf3eeb604521f4c7c314e5ee62a7864
aisha-miranda <tashchanova@bk.ru>
2025-04-06 00:00:21 +0500

    .

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,30 @@ import service.TaskManager;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManager manager = new TaskManager();
+        TaskManager taskManager = new TaskManager();
 
         Epic firstEpic = new Epic("Эпик 1 (с двумя подзадачами)", "Описание эпика 1");
-        manager.createEpic(firstEpic);
+        taskManager.createEpic(firstEpic);
 
         Subtask firstEpicFirstSubtask = new Subtask("Подзадача 1 эпика 1", "Описание подзадачи 1 эпика 1", firstEpic);
         Subtask firstEpicSecondSubtask = new Subtask("Подзадача 2 эпика 1", "Описание подзадачи 2 эпика 1", firstEpic);
-        manager.createSubtask(firstEpicFirstSubtask);
-        manager.createSubtask(firstEpicSecondSubtask);
+        taskManager.createSubtask(firstEpicFirstSubtask);
+        taskManager.createSubtask(firstEpicSecondSubtask);
 
         Epic secondEpic = new Epic("Эпик 2 (с одной подзадачей)", "Описание эпика 2");
-        manager.createEpic(secondEpic);
+        taskManager.createEpic(secondEpic);
 
         Subtask secondEpicFirstSubtask = new Subtask("Подзадача 1 эпика 2", "Описание подзадачи 1 эпика 2", secondEpic);
-        manager.createSubtask(secondEpicFirstSubtask);
+        taskManager.createSubtask(secondEpicFirstSubtask);
 
         Task firstTask = new Task("Задача 1", "Описание задачи 1");
         Task secondTask = new Task("Задача 2", "Описание задачи 2");
-        manager.createTask(firstTask);
-        manager.createTask(secondTask);
+        taskManager.createTask(firstTask);
+        taskManager.createTask(secondTask);
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getSubtasks());
 
         System.out.println("Изначальный статус эпика: " + firstEpic.getStatus());
 
@@ -39,11 +39,11 @@ public class Main {
         firstEpicSecondSubtask.setStatus("DONE");
         System.out.println("Статус эпика после изменения статуса подзадач на \"DONE\": " + firstEpic.getStatus());
 
-        manager.removeTaskById(6);
-        manager.removeEpicById(4);
+        taskManager.removeTaskById(6);
+        taskManager.removeEpicById(4);
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getSubtasks());
     }
 }
