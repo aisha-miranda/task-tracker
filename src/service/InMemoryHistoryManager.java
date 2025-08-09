223abb6ec59a503f0fc52c25052509c7de31b66c
aisha-miranda <tashchanova@bk.ru>
2025-08-10 00:13:07 +0500

    Created HashMap that keeps task browsing history; add() method now deletes the task from history if it was browsed previously, and adds it to the end of linked list

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -3,13 +3,15 @@ package service;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int MAX_HISTORY_SIZE = 10;
     public Node head;
     public Node tail;
-    private final List<Task> history = new ArrayList<>();
+    private int size = 0;
+    HashMap<Integer, Node> map = new HashMap<>();
 
     class Node {
         Task task;
@@ -32,16 +34,17 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
         tail = newNode;
         size++;
+        map.put(task.getId(), newNode);
     }
 
-    public void removeNode(Node node){
-        if (node==null){
+    public void removeNode(Node node) {
+        if (node == null) {
             return;
         }
         if (node.prev == null) {
             head = node.next;
             head.prev = null;
-        } else if (node.next == null){
+        } else if (node.next == null) {
             tail = node.prev;
             tail.next = null;
         } else {
@@ -49,6 +52,7 @@ public class InMemoryHistoryManager implements HistoryManager {
             node.next.prev = node.prev;
         }
         size--;
+        map.remove(node.task.getId());
     }
 
     public List<Task> getTasks() {
@@ -64,16 +68,26 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void add(Task task) {
         if (task != null) {
-            // Если размер в истории достиг максимального, удаляем самую старую задачу
-            if (history.size() >= MAX_HISTORY_SIZE) {
-                history.remove(0);
+            if (map.size() >= MAX_HISTORY_SIZE) {
+                map.remove(0);
             }
-            history.add(task);
+            Node node = map.get(task.getId());
+            if (node != null) {
+                removeNode(node);
+            }
+            linkLast(task);
+        }
+    }
+
+    public void remove(Task task) {
+        if (task != null) {
+            Node node = map.get(task.getId());
+            removeNode(node);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(history);
+        return getTasks();
     }
 }
